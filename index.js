2390e874a01bc682f43d3689f98215c6947238a7
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-08 11:36:10 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,182 @@
+
+//botid: 508122813299818511
+//Link: https://discordapp.com/oauth2/authorize?client_id=508122813299818511&scope=bot&permissions=8
+
+
+//call certain functions
+const Discord = require("discord.js");
+const config = require("./config.json");
+let token = config.token;
+const bot = new Discord.Client();
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+const ownerID = '507408804145528832';
+let blue = config.blue;
+let cooldown = new Set();
+let cdseconds = 3
+let active = new Map();
+let grey = config.grey;
+var enabled = false
+let TOKEN = process.env.TOKEN;
+const db = require("quick.db")
+//let xp = require('./xp.json');
+const DBL = require('dblapi.js');
+
+
+//to load the commands
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js");
+  if(jsfile.length <= 0){
+    console.log("Couldn't find commands.");
+    return;
+  }
+
+jsfile.forEach((f, i) =>{
+  delete require.cache[require.resolve(`./commands/${f}`)];
+  let props = require(`./commands/${f}`);
+  console.log(`${f} has loaded!`);
+  bot.commands.set(props.help.name, props);
+});
+
+
+bot.on("message", async message => {
+  //a little bit of data parsing/general checks
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+  let content = message.content.split(" ");
+  let command = content[0];
+  let args = content.slice(1);
+var prefix = '>>';
+let fetched = await db.fetch(`prefix_${message.guild.id}`);
+if (fetched === null) prefix = '>>';
+else prefix = fetched;
+  if(!message.content.startsWith(prefix)) return;
+
+
+
+//Cooldown commands in Chat (ill add settings soon.)
+if(cooldown.has(message.author.id)){
+  message.delete()
+  let cooldown = new Discord.RichEmbed()
+  .setTitle(message.author.username)
+  .setDescription(`Woah, Woah... Slow down your requests!`)
+  .setFooter('Please wait 3 seconds before using a command')
+  .setColor(grey)
+  return message.channel.send(cooldown)
+}
+if (!["507408804145528832"].includes(message.author.id)) {
+  cooldown.add(message.author.id);
+
+}
+
+  // Ops to transfer
+  let ops = {
+    ownerID: ownerID,
+    blue: blue,
+    active: active,
+    grey: grey,
+    enabled: enabled
+  }
+   
+  //checks if message contains a command and runs it
+  let commandfile = bot.commands.get(command.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args,ops);
+
+  setTimeout(() => {
+    cooldown.delete(message.author.id)
+  }, cdseconds * 1000)
+   
+
+
+});
+
+});
+
+
+
+
+bot.on('message', async message => {
+ if (message.content.startsWith(">>toggle bwords")) {
+message.delete()
+    if(enabled === true) {
+        enabled = false
+      let on = new Discord.RichEmbed()
+.setDescription("Blacklisted Words has been **DISABLED** :negative_squared_cross_mark:")
+      .setColor(0x36393f)
+      message.channel.send(on)
+    }else{
+  message.delete()
+      enabled = true
+            let off = new Discord.RichEmbed()
+.setDescription("Blacklisted Words has been **ENABLED** :white_check_mark:")
+      .setColor(0x36393f)
+      message.channel.send(off)
+    }
+  }
+  
+  //1 blacklisted words
+  let blacklisted = ['fuck','bullshit','bitch','asshole','cunt','virgin','damn','fucker','fucked','raped','rape','ass','stfu','wtf','tf','fck','fk','bs','shit','omfg','tit','cum','dick','vagina'] //words
+
+  //2 looking for words
+  let foundInText = false;
+  for (var i in blacklisted) { // loops through the blacklisted list
+    if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase())) foundInText = true;
+  }
+ 
+  //3 deletes and send message
+    if (foundInText) {
+      if (["507408804145528832"].includes(message.author.id)) return
+      if(enabled) {
+       let reply = new Discord.RichEmbed()
+      .setColor(0x36393f)
+     .setDescription(`${message.author} watch your language :rage:`)
+      message.channel.send(reply).then(msg => msg.delete(5000));
+      message.delete()
+    }else{
+      foundInText = false
+      return;
+    }
+}
+})
+  
+//Statuses
+bot.on('ready', () => { 
+  console.log(`${bot.user.username} is watching ${bot.guilds.size} Discord Servers and ${bot.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Users!`);
+	let Activities = [
+    `Type >>help`
+                  
+	];
+	setInterval(function() {
+		let activity = Activities[Math.floor(Math.random() * Activities.length)];
+		bot.user.setActivity(activity, { type: 'WATCHING' }); //
+  }, 10000);
+});
+
+bot.reminders = new Map();
+bot.once('ready', function() {
+    setInterval(() => {
+        for (const [key, value] of this.reminders) {
+            if (value.time < Date.now()) {
+                this.channels.get(value.channel).send(value.message);
+                this.reminders.delete(key);
+            };
+        };
+    }, 5000);
+});
+//Guild Invite Join
+bot.on('guildCreate', guild => {
+let welcome = new Discord.RichEmbed()
+    welcome.setTitle("😄 Thank you for inviting me to your server! 😄")
+  welcome.setDescription("I was made by Apollyon365#3607")
+    welcome.setColor(0x36393f)
+    welcome.addField("Links", "🔗 [Invite Me](https://discordapp.com/oauth2/authorize?client_id=508122813299818511&scope=bot&permissions=2146958847)\n ❔ [Support Server](https://discord.gg/7xuEzbt)\n 🔺 [Vote for Void](https://discordbots.org/bot/508122813299818511/vote) \n 💰 [Donate for Void+](https://donatebot.io/checkout/515737847957946388)");
+    welcome.addField("Prefix", "<:vmore:517907860861222923> Prefix for this bot is ``>>``\nIf you wish to change it please do >>setprefix <prefix>\nIf you forget your prefix mention the bot")
+  welcome.addField("Help", "<:vhelp:517498464628768778> Do ``>>help`` for the commands!")
+    guild.channels.filter(c => c.type === 'text').first().send(welcome);
+});
+
+
+bot.login(TOKEN)

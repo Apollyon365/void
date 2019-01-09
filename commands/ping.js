1960304b61f382fc38fd38330826df41910bd385
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-09 04:18:25 -0700

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args, ops) => {
+  let grey = ops.grey
+  let pong = new Discord.RichEmbed()
+ 
+  .setDescription("🏓 Pong! \n ```diff\n" + `- - - ${Math.round(bot.ping)}ms` + `\n- - -\`\`\``)
+  .setColor(grey)
+  
+   message.channel.send(pong);        
+     
+   
+}
+
+exports.help = {
+  name: 'ping',
+  description: 'Shows the ping of the bot!',
+  usage: 'rping',
+  inHelp: 'yes'
+};
+
+exports.conf = {
+  aliases: []
+};
+module.exports.help = {
+name: "ping"
+}

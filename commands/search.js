d9405a18e4eb3d5b38eb7dc9646ed5da47276421
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-09 04:19:51 -0700

    Create search.js

---
+++ commands/search.js
@@ -0,0 +1,50 @@
+const search = require('yt-search');
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args, ops) => {
+  message.delete()
+let error = new Discord.RichEmbed()
+    .setColor(0x36393f)
+    .setDescription('Something went wrong.');
+ 
+  
+
+    search(args.join(' '), function(err, res) {
+      if (err) return message.channel.send(error).then(msg => msg.delete(5000));
+
+      let videos = res.videos.slice(0, 5);
+
+      let resp = '';
+      for (var i in videos) {
+        resp += `**${parseInt(i)+1}** - ${videos[i].title}\n`;
+      }
+
+      resp += `\n**Choose the number between 1-${videos.length}**`;
+let search = new Discord.RichEmbed()
+.setTitle(`Results for ${args.join(' ')}`)
+    .setColor(0x36393f)
+    .setDescription(resp);
+     message.channel.send(search);
+      
+     /* const filterz = m => m.content.startsWith('b!play');
+      
+      message.channel.awaitMessages(filterz, { max: 1, time: 60000, errors: ['time'] })
+  .then(collected => console.log(collected.size))
+  .catch(collected => console.log(`Type ``cancel`` to cancel the search`)); */
+
+      const filter = m => !isNaN(m.content) && m.content < videos.length+1 && m.content > 0;
+
+      const collector = message.channel.createMessageCollector(filter);
+
+      collector.videos = videos;
+
+      collector.once('collect', function(m) {
+
+        let commandFile = require(`./play.js`);
+        commandFile.run(bot, message, [this.videos[parseInt(m.content)-1].url], ops);
+      });
+    });
+}
+module.exports.help = {
+  name: "search"
+}

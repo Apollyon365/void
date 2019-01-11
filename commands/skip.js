3590058d1b6cbeaa5ec6ee64e2386d8f33cc2fec
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-11 11:36:22 -0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,6 +1,18 @@
 const Discord = require('discord.js')
+const DBL = require('dblapi.js')
 module.exports.run = async (bot, message, args, ops) => {
 message.delete()
+  let novote = Discord.RichEmbed()
+  .setDescription("Sorry you must vote for Void before using this command, this is for Performance Issues\nClick [here](https://discordbots.org/bot/508122813299818511/vote) to vote for Void! Thank you!")
+  .setColor(0x36393f)
+  .setFooter("P.S if you have voted for Void please wait a few minutes before trying again")
+ let user = message.author
+const voted = await dbl.hasVoted(user.id);
+  if (voted) {
+    console.log('voted yo') 
+  } else {
+    message.channel.send(novote)
+  }
      let fetched = ops.active.get(message.guild.id);
 let error = new Discord.RichEmbed()
     .setColor(0x36393f)

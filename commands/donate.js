e5edafc485dd5d9806592af83ad83de7e24277cf
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-18 08:41:02 -0500

    Update donate.js

--- commands/donate.js
+++ commands/donate.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args, ops) => {
 let donate = new Discord.RichEmbed()
     .setColor(0x36393f)
     .setTitle("Donation")
-    .setDescription("Rules if you want to donate:\n1. You will not get any special perks besides a rank in Void's Support Server.\n2. You will not judge my name.\n 3. A chargeback will result in a blacklist towards Void\n4. Lies about fake donations will result in a blacklist towards Void\n5. You are the coolest person in your class **IF** you donate..")
+    .setDescription("Rules if you want to donate:\n1. You will not get any special perks besides a rank in Void's Support Server.\n2. You will not judge my name.\n 3. A chargeback will result in a blacklist towards Void\n4. Lies about fake donations will result in a blacklist towards Void\n5. You are the coolest person in your class **IF** you donate..\nK now you have read the rules you can donate [here](https://paypal.me/apollyon365)")
    message.channel.send(donate)
    }
 module.exports.help = {

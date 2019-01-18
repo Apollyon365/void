55dd903782da6582a4044dcf5102e34662f62b51
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-18 07:48:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ let welcome = new Discord.RichEmbed()
     welcome.setColor(0x36393f)
     welcome.addField("Links", "🔗 [Invite Me](https://discordapp.com/oauth2/authorize?client_id=508122813299818511&scope=bot&permissions=2146958847)\n ❔ [Support Server](https://discord.gg/7xuEzbt)");
     welcome.addField("Prefix", "<:vmore:517907860861222923> Prefix for this bot is ``>>``\nIf you wish to change it please do >>setprefix <prefix>\nIf you forget your prefix mention the bot")
-  welcome.addField("Help", "<:vhelp:517498464628768778> Type ``>>help`` for the commands!")
+  welcome.addField("Help", "Type ``>>help`` for the commands!")
     guild.channels.filter(c => c.type === 'text').first().send(welcome);
 });
 

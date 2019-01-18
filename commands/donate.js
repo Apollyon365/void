f30d828787f845303c8d36a3ee293f1cffdd3390
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-18 08:38:38 -0500

    YAY

---
+++ commands/donate.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args, ops) => {
+  message.delete()
+let donate = new Discord.RichEmbed()
+    .setColor(0x36393f)
+    .setTitle("Donation")
+    .setDescription("Rules if you want to donate:\n1. You will not get any special perks besides a rank in Void's Support Server.\n2. You will not judge my name.\n 3. A chargeback will result in a blacklist towards Void\n4. Lies about fake donations will result in a blacklist towards Void\n5. You are the coolest person in your class **IF** you donate..")
+   message.channel.send(donate)
+   }
+module.exports.help = {
+  name: "donate"
+}

e72436f8edd339e15886143f668f566398c49bb6
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-22 16:03:50 -0700

    Update calc.js

--- commands/calc.js
+++ commands/calc.js
@@ -1,24 +1,11 @@
 const Discord = require("discord.js");
 const math = require("mathjs");
-const DBL = require('dblapi.js');
 exports.run = async (bot, message, args, ops) => {
   message.delete()
 
    const equation = new Discord.RichEmbed()
     .setColor(0x36393f)
    .setDescription("What is your equation?")
-           let novote = new Discord.RichEmbed()
- .setColor(0x36393f)
-  .setDescription("Woah Woah.. before you can do your homework \n this command is for DBL voters only! \nYou can vote by clicking [here](https://discordbots.org/bot/508122813299818511/vote)")
-
-    const dbltoken = process.env.DBL
-  const dbl = new DBL(dbltoken, { webhookPort: 5000, webhookAuth: 'password' });
-const voted = await dbl.hasVoted(message.author.id);
-  if (voted) {
-    if (!args[0]) return message.channel.send(equation) 
-  } else {
-    message.channel.send(novote)
-  }
 
   
   let resp;

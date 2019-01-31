2e9e6adcc588986836220d54a2c8cb205f2e31c5
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-30 23:44:21 -0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,34 +1,22 @@
 const Discord = require('discord.js')
-const DBL = require('dblapi.js')
 module.exports.run = async (bot, message, args, ops) => {
 message.delete()
-  let novote = Discord.RichEmbed()
-  .setDescription("Sorry you must vote for Void before using this command, this is for Performance Issues\nClick [here](https://discordbots.org/bot/508122813299818511/vote) to vote for Void! Thank you!")
-  .setColor(0x36393f)
-  .setFooter("P.S if you have voted for Void please wait a few minutes before trying again")
- let user = message.author
-const voted = await dbl.hasVoted(user.id);
-  if (voted) {
-    console.log('voted yo') 
-  } else {
-    message.channel.send(novote)
-  }
      let fetched = ops.active.get(message.guild.id);
 let error = new Discord.RichEmbed()
     .setColor(0x36393f)
-    .setDescription('There is currently no music playing in the guild!');
-     if (!fetched) return message.channel.send(error);
+    .setDescription('🚫 There must be music playing to use that');
+     if (!fetched) return message.channel.send('🚫 There must be music playing to use that!');
   let nosame = new Discord.RichEmbed()
     .setColor(0x36393f)
-    .setDescription('You are not in the same channel as me.');
+    .setDescription('🚫 You are not in the same channel as me.');
 
-     if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send(nosame);
+     if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send('🚫 You are not in the same channel as me.');
   
 let skip = new Discord.RichEmbed()
     .setColor(0x36393f)
-    .setDescription('Sucessfully skipped the song!');
+    .setDescription('⏭ Sucessfully skipped the song!');
 
-       message.channel.send(skip);
+       message.channel.send('⏭ Sucessfully skipped the song!');
 
        return fetched.dispatcher.emit('end');
 

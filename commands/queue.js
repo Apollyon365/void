92df3c73f50ae4a15ef59835cb93c70bb67d7018
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-30 23:38:42 -0700

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -4,17 +4,17 @@ message.delete()
    let fetched = ops.active.get(message.guild.id);
    let error = new Discord.RichEmbed();
     error.setColor(0x36393f)
-    error.setDescription('Queue list is empty.');
+    error.setDescription('🚫 Queue list is empty.');
     //----------------------
 
 
 
-   if (!fetched) return message.channel.send(error)
+   if (!fetched) return message.channel.send("🚫 Queue list is empty.")
 
    let queue = fetched.queue;
    let nowPlaying = queue[0];
 
-   let resp = `__**Currently Playing**__\n**${nowPlaying.songTitle}** -- Requested by: *${nowPlaying.requester}*\n\n__**Queue**__\n`;
+   let resp = `🎶 **Currently Playing** 🎶\n**${nowPlaying.songTitle}** -- Requested by: *${nowPlaying.requester}*\n\n__**Queue**__\n`;
 
    for (var i = 1; i < queue.length; i++) {
      resp += `${i}. **${queue[i].songTitle}** -- Requested by: *${queue[i].requester}*\n`;
@@ -22,7 +22,7 @@ message.delete()
     let qsueue = new Discord.RichEmbed();
     qsueue.setColor(0x36393f)
     qsueue.setDescription(resp);
-        message.channel.send(qsueue);
+        message.channel.send(resp);
 
    }
 module.exports.help = {

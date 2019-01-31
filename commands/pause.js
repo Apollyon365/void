319a2607c20e2285cd34a7c28a2898dcdccfb12c
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-30 23:37:57 -0700

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -5,24 +5,24 @@ message.delete()
    let fetched = ops.active.get(message.guild.id);
 let error = new Discord.RichEmbed()
     .setColor(0x36393f)
-    .setDescription('There is currently no music playing in the guild!');
-   if (!fetched) return message.channel.send(error);
+    .setDescription('🚫 There must be music playing to use that!');
+   if (!fetched) return message.channel.send('🚫 There must be music playing to use that!');
 let nosame = new Discord.RichEmbed()
     .setColor(0x36393f)
-    .setDescription('We\'re not in the same voice channel');
-   if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send(nosame)
+    .setDescription('🚫 We\'re not in the same voice channel');
+   if (message.member.voiceChannel !== message.guild.me.voiceChannel) return message.channel.send('🚫 We\'re not in the same voice channel')
 let paused = new Discord.RichEmbed()
     .setColor(0x36393f)
-    .setDescription('The music is already paused.');
-   if (fetched.dispatcher.paused) return message.channel.send(paused)
+    .setDescription('🚫 The music is already paused.');
+   if (fetched.dispatcher.paused) return message.channel.send('🚫 The music is already paused.')
 
    fetched.dispatcher.pause()
 
    let pausedembed = new Discord.RichEmbed()
-   .setDescription(`${message.author.username} has paused the music`)
+   .setDescription(`⏯ ${message.author.username} has paused the music`)
    .setColor(0x36393f)
 
-   message.channel.send(pausedembed)
+   message.channel.send(`⏯ ${message.author.username} has paused the music`)
 
 
    }

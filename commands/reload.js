2156489213b1d7c260b07d5b576579c0f71376d7
Apollyon365 <46497600+Apollyon365@users.noreply.github.com>
2019-01-31 21:58:23 -0700

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args, ops) => {
 let grey = ops.grey
-message.delete()
+
     let NotDev = new Discord.RichEmbed()
     .setColor(grey)
     .setTitle("Reload Command")
